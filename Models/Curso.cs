e4d0a771eb0e751a0c8e775d5bf5859fbb369e3c
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2023-11-16 18:17:18 -0300

    implementando a classe curso

---
+++ Models/Curso.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploExplorando.Models
+{
+    public class Curso
+    {
+        public string Nome { get; set; }
+        public List<Pessoa> Alunos { get; set; }
+
+        public void AdicionarAluno(Pessoa aluno)
+        {
+            Alunos.Add(aluno);
+        }
+    }
+}
