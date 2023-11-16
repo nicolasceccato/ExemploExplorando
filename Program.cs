fd1e5984027f1c2b8cdbf4a3b0bdbaafe391ea94
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2023-11-16 16:22:08 -0300

    iniciando

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa();
+
+p1.Nome = "Fulano";
+p1.Idade = 20;
+p1.Apresentar();
