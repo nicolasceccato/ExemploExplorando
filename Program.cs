b16edf6cc17694b177d8be1e823722d16d4a4fad
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2023-11-18 14:41:17 -0300

    lendo arquivo

--- Program.cs
+++ Program.cs
@@ -1,28 +1,35 @@
-﻿using ExemploExplorando.Models;
+﻿// using ExemploExplorando.Models;
+//
+// Pessoa p1 = new Pessoa();
+//
+// p1.Nome = "Fulano";
+// p1.Sobrenome = "Ciclano";
+// // p1.Idade = 20;
+// // p1.Apresentar();
+//
+// Pessoa p2 = new Pessoa();
+// p2.Nome = "Taco";
+// p2.Sobrenome = "Bell";
+//
+// Curso cursoDeIngles = new Curso();
+// cursoDeIngles.Nome = "Ingles";
+// cursoDeIngles.Alunos = new List<Pessoa>();
+//
+// Pessoa p3 = new Pessoa(nome: "Ronald", sobrenome: "McDonald");
+//
+// cursoDeIngles.AdicionarAluno(p1);
+// cursoDeIngles.AdicionarAluno(p2);
+// cursoDeIngles.AdicionarAluno(p3);
+//
+// cursoDeIngles.ListarAlunos();
+//
+// double porcent = .3421;
+//
+// Console.WriteLine(porcent.ToString("P"));
 
-Pessoa p1 = new Pessoa();
+string[] linhas = File.ReadAllLines("arquivos/arquivoLeitura.txt");
 
-p1.Nome = "Fulano";
-p1.Sobrenome = "Ciclano";
-// p1.Idade = 20;
-// p1.Apresentar();
-
-Pessoa p2 = new Pessoa();
-p2.Nome = "Taco";
-p2.Sobrenome = "Bell";
-
-Curso cursoDeIngles = new Curso();
-cursoDeIngles.Nome = "Ingles";
-cursoDeIngles.Alunos = new List<Pessoa>();
-
-Pessoa p3 = new Pessoa(nome: "Ronald", sobrenome: "McDonald");
-
-cursoDeIngles.AdicionarAluno(p1);
-cursoDeIngles.AdicionarAluno(p2);
-cursoDeIngles.AdicionarAluno(p3);
-
-cursoDeIngles.ListarAlunos();
-
-double porcent = .3421;
-
-Console.WriteLine(porcent.ToString("P"));
+foreach (string linha in linhas)
+{
+    Console.WriteLine(linha);
+}
